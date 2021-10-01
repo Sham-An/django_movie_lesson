f7e16e0947697208ec2e283cc1086d4087759c04
Sham-An <shamraev@inbox.ru>
2021-10-01 21:23:59 +0300

    урок 7 ЗАКОНЧЕН!!! 9:40 Построение ссылки. Вскипает мосх https://youtu.be/NxBlcfJQSNk?t=582

--- movies/views.py
+++ movies/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
-from django.views.generic.base import View
+from django.views.generic import ListView, DetailView
+#from django.views.generic.base import View
+
 from .models import Movie
 
 
 # Create your views here.
 
-class MoviesView(View):
+class MoviesView(ListView):
 
     """список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False) #all()
+    #template_name = "movies/movies.html" переименовано в movie_list.html
+
+# (View)
+#    def get(self, request):
+#        movies = Movie.objects.all()
+#        return render(request, "movies/movies.html", {"movie_list": movies})
 
 
-class MovieDetailView(View):
+class MovieDetailView(DetailView):
 
     """Полное описание фильмов"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, "movies/movie_detail.html", {"movie": movie})
+    model = Movie
+    slug_field = "url"
+#(View)
+#    def get(self, request, slug):
+#        movie = Movie.objects.get(url=slug)
+#        return render(request, "movies/movie_detail.html", {"movie": movie})
